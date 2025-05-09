3c19df513f458076940a72081591cec7473febe8
RobM_Tech <robert.mathieu.tech@gmail.com>
2025-05-09 14:17:43 -0500

    C: added rotation to player

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame, circleshape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
-class Player(circleshape.CircleShape):
+class Player(circleshape.CircleShape): # inherit player hit box
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -9,6 +9,7 @@ class Player(circleshape.CircleShape):
     
     # in the player class
     def triangle(self):
+        # player avatar
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
@@ -18,3 +19,17 @@ class Player(circleshape.CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
+
+    
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+            
+        if keys[pygame.K_d]:
+            self.rotate(dt)
