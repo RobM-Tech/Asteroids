13e791e1c9a3c3890a088b9763c308c39c494cc4
RobM_Tech <robert.mathieu.tech@gmail.com>
2025-05-09 12:57:12 -0500

    B: Added screen, player, and circleshape.No movement

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+        
 
 if __name__ == "__main__":
     main()
